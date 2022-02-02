acd923063f8feaaa228d891f580718716c8474f9
Tetiana-Vakhovska <benedyktanya@ukr.net>
2022-02-02 22:46:38 +0300

    Update 01-gallery.js
    01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -39,9 +39,13 @@ function onPaletteContainerClick(evt) {
 }
 function removeActiveClass() {
    const currentActiveCard = document.querySelector(`.color-card.is-active`);
-    if (currentActiveCard) {
-        currentActiveCard.classList.remove(`.is-active`);
-    }
+  if (currentActiveCard) {
+    currentActiveCard.classList.remove(`.is-active`);
+  }
+      const instance = basicLightbox.create(`
+    <img src="assets/images/image.png" width="800" height="600">
+`);
+    
 }
 function addActiveCardClass(card) {
   card.classList.add(`.is-active`);
