001434e3ffddcf58db0e7ceb3445ee390e557006
Tetiana-Vakhovska <benedyktanya@ukr.net>
2022-02-07 13:58:19 +0300

    Update 02-lightbox.js
    02js

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -2,3 +2,18 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const listItem = document.querySelector(".gallery");
+const cardListItem = ({ original, preview, description }) => {
+    return `<a class="gallery__item" href="${original}"">
+  <img class="gallery__image" src="${ preview}" alt="${description}" />
+</a>`};
+const cardListItemNew = galleryItems.map(cardListItem).join("");
+const cardsMarkup = cardListItem (galleryItems);
+listItem.addEventListener(`click`, onPaletteContainerClick);
+listItem.insertAdjacentHTML("beforeend", cardListItemNew);
+console.log(cardListItemNew); 
+function onPaletteContainerClick(evt) {
+    evt.preventDefault()
+   
+}
+ let instance= SimpleLightbox('.gallery',{captionsData:'alt', captionDelay:250});
