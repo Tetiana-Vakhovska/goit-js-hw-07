fdc978804a0edd00df8e775add98e8b6c5082e64
Tetiana-Vakhovska <benedyktanya@ukr.net>
2022-02-08 23:59:27 +0300

    Update 02-lightbox.js
    02

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,28 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
-
 console.log(galleryItems);
 const listItem = document.querySelector(".gallery");
 const cardListItem = ({ original, preview, description }) => {
-    return `<a class="gallery__item" data-lightbox="images" href="${original}"">
+    return `<a class="gallery__item"  href="${original}"">
   <img class="gallery__image" src="${ preview}" alt="${description}" />
 </a>`};
-
 const cardListItemNew = galleryItems.map(cardListItem).join("");
-const cardsMarkup = cardListItem(galleryItems);
-const lightbox = document.createElement('div');
-lightbox.id = 'lightbox';
-document.body.appendChild(lightbox);
 listItem.addEventListener(`click`, onPaletteContainerClick);
 listItem.insertAdjacentHTML("beforeend", cardListItemNew);
 function onPaletteContainerClick(evt) {
-  evt.preventDefault()
-    
-}
+  evt.preventDefault()   
+}console.log(cardListItemNew);
+const lightbox = document.createElement('div.gallery');
+lightbox.id = 'lightbox';
+document.body.appendChild(lightbox);
+const images = document.querySelectorAll('img');
+images.forEach(image => {
+  image.addEventListener('click',
+    e => {
+      lightbox.classList.add('active')
+      const img = document.createElement('img')
+      img.scr = image.scr;
+      while (lightbox.firstChild) {
+        lightbox.removeChild(lightbox.firstChild)
+      }
+    lightbox.appendChild(img)
+    })
+})
+lightbox.addEventListener('click',
+  e => { if(e.target!==e.currentTarget) return
+  lightbox.classList.remove('active')
+})
 
-
-let gallery = new SimpleLightbox('.lightbox', {
-  captionsData: `alt`, captiondelay: 250,
-  captionPosition:bottom
-});
-
+console.log(lightbox);
