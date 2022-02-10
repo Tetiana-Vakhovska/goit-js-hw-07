c832f6ec21bccbd1721c5591e5201511213ba009
Tetiana-Vakhovska <benedyktanya@ukr.net>
2022-02-10 23:14:56 +0300

    Update 02-lightbox.js
    02

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,19 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
 console.log(galleryItems);
 const listItem = document.querySelector(".gallery");
-const lightbox = document.createElement('div.gallery');
-lightbox.id = 'lightbox';
-document.body.appendChild(lightbox);
-open.simplelightbox
 const cardListItem = ({ original, preview, description }) => {
-    return `<a class="gallery__item"  href="${original}"">
-  <img class="gallery__image" src="${ preview}" alt="${description}" />
-</a>`};
+    return `<div class="gallery,SimpleLightbox">
+  <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${ preview}"
+    
+          alt="${description}"
+        />
+        </a>
+</div>`;
+};
 const cardListItemNew = galleryItems.map(cardListItem).join("");
+const cardsMarkup = cardListItem (galleryItems);
 listItem.addEventListener(`click`, onPaletteContainerClick);
 listItem.insertAdjacentHTML("beforeend", cardListItemNew);
-cardListItemNew.insertAdjacentHTML("beforeend",lightbox)
+
+
 function onPaletteContainerClick(evt) {
-  evt.preventDefault()   
-}console.log(lightbox);
+  evt.preventDefault()
+}
+let gallery = new SimpleLightbox('.gallery ', {
+ captionsData: `alt`, captiondelay: 250,
+  captionPosition:bottom
+});
+show.simplelightbox()
