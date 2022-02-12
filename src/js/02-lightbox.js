b1855da82bb552c6479f6f9924ade558199c61ca
Tetiana-Vakhovska <benedyktanya@ukr.net>
2022-02-12 22:46:05 +0300

    Update 02-lightbox.js
    js02

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,23 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
-const listItem = document.querySelector(".gallery");
-
-const cardListItemNew = galleryItems.map(cardListItem).join("");
-const cardsMarkup = cardListItem (galleryItems);
-listItem.addEventListener(`click`, onPaletteContainerClick);
-listItem.insertAdjacentHTML("beforeend", cardListItemNew);
-
-
-function onPaletteContainerClick(evt) {
-  evt.preventDefault()
+const ref = {
+  galleryListHtml: document.querySelector('.gallery'),
+  galleryImageLigthBox:document.querySelector('body')
 }
-//let gallery = new SimpleLightbox('.gallery a', {
- //captionsData: `alt`, captiondelay: 250,
-  //captionPosition:bottom
-//});
+ref.galleryImageLigthBox.addEventListener('clic', tupGalleryItemCard)
+function galleryItemCard (lightbox) {
+  return lightbox.map(e => {
+    return `<a  class="gallery__item" href="${e.original}">
+        <img
+          class="gallery__image"
+          src="${
+          e.preview}"
+          alt="${e.description}"
+        />
+      </a>`
+  })
+  .join('')
+}
+ref.galleryListHtml.insertAdjacentHTML('befforend', galleryItemCard(galleryItems));
+function tupGalleryItemCard(e) {
+  if (e.target.nodeName === 'IMG') {
+    e.preventDefault()
+    new SimpleLightbox('.gallery a', {
+      captionsData: `alt`, captiondelay: 250,
+      captionPosition: bottom
+    });
 
-console.log(lightbox);
-var gallery = $('.gallery a').simpleLightbox();
-console.log(gallery);
+  }
+}
